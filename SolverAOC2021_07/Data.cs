6121fd9b11b9fcb69558f7b1e67924645e75f2cd
Ondřej Brožek <obrozek@centrum.cz>
2021-12-07 09:06:54 +0100

    7

---
+++ SolverAOC2021_07/Data.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolverAOC2021_07
+{
+  class Data
+  {
+
+    List<int> Input;
+
+    public Data(string input)
+    {
+      using(StringReader sr = new StringReader(input))
+      {
+        string line = sr.ReadLine();
+        var arr = line.Split(',');
+        Input = new List<int>(arr.Select(x=>int.Parse(x)));
+      }
+
+    }
+
+    public int Solve1()
+    {
+      int res = int.MaxValue;
+      
+      for (int i = Input.Min(); i <= Input.Max(); i++)
+      {
+        int resTmp = 0;
+        foreach (int val in Input)
+        {
+          resTmp += Math.Abs(i - val);
+        }
+
+        if (resTmp < res)
+        {
+          res = resTmp;
+        }
+      }
+      return res;
+    }
+
+
+    public int Solve2()
+    {
+      int res = int.MaxValue;
+
+      for (int i = Input.Min(); i <= Input.Max(); i++)
+      {
+        int resTmp = 0;
+        foreach (int val in Input)
+        {
+          int dist = Math.Abs(i - val);
+          int fuel = ((1 + dist) * dist) / 2;
+          resTmp += fuel;
+        }
+
+        if (resTmp < res)
+        {
+          res = resTmp;
+        }
+      }
+      return res;
+    }
+
+  }
+}
--- SolverAOC2021_07/Program.cs
+++ SolverAOC2021_07/Program.cs
@@ -10,18 +10,23 @@ namespace SolverAOC2021_07
   public class Program : BaseAdventSolver, IAdventSolver
   {
 
-    public override string SolverName => "SolverAOC2021_07"/*TODO: Task Name*/;
+    public override string SolverName => "Day 7: The Treachery of Whales"/*TODO: Task Name*/;
 
     public override string InputsFolderName => "SolverAOC2021_07";
 
     public override string SolveTask1(string InputData)
     {
-      throw new NotImplementedException();
+      Data d = new Data(InputData);
+      int res = d.Solve1();
+      return res.ToString();
+      
     }
 
     public override string SolveTask2(string InputData)
     {
-      throw new NotImplementedException();
+      Data d = new Data(InputData);
+      int res = d.Solve2();
+      return res.ToString();
     }
   }
 }
