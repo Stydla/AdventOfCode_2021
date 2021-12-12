ef9a2581da20f3669ba321cae25c2b13bc619044
Ondra <Ondra@ONDRA-PC>
2021-12-12 09:29:46 +0100

    12

---
+++ SolverAOC2021_12/Data.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolverAOC2021_12
+{
+  internal class Data
+  {
+
+    public List<Node> AllNodes;
+
+    public Data(string input)
+    {
+      AllNodes = new List<Node>();
+      using(StringReader sr = new StringReader(input))
+      {
+        string line;
+        while((line = sr.ReadLine()) != null)
+        {
+          string[] arr = line.Split('-');
+          Node n1 = GetNode(arr[0]);
+          Node n2 = GetNode(arr[1]);
+          n1.Link(n2);
+        }
+      }
+    }
+
+    internal int Solve1()
+    {
+      Node startCave = AllNodes.First(x => x.IsStartCave);
+      int res = startCave.Search(true);
+      return res;
+    }
+
+    internal int Solve2()
+    {
+      Node startCave = AllNodes.First(x => x.IsStartCave);
+      int res = startCave.Search(false);
+      return res;
+    }
+
+    private Node GetNode(string name)
+    {
+      Node n = AllNodes.FirstOrDefault(x => x.Name == name);
+      if (n != null)
+      {
+        return n;
+      }  
+      n = new Node(name);
+      AllNodes.Add(n);
+      return n;
+    }
+  }
+}
---
+++ SolverAOC2021_12/Node.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolverAOC2021_12
+{
+  internal class Node
+  {
+    public List<Node> Nodes = new List<Node>();
+
+    public string Name;
+    public bool IsBigCave;
+    public bool IsStartCave;
+    public bool IsEndCave;
+
+    public int state;
+
+    public Node(string name)
+    {
+      this.Name = name;
+      IsBigCave = Name[0] <= 'Z';
+      IsStartCave = Name == "start";
+      IsEndCave = Name == "end";
+    }
+
+    internal void Link(Node n2)
+    {
+      this.Nodes.Add(n2);
+      n2.Nodes.Add(this);
+    }
+
+    public int Search(bool visitedTwice)
+    {
+      if(!IsBigCave && state > 1)
+      {
+        return 0;
+      }
+      if(!IsBigCave && state > 0 && visitedTwice)
+      {
+        return 0;
+      }
+      if(IsEndCave)
+      {
+        return 1;
+      }
+      if(IsStartCave && state > 0)
+      {
+        return 0;
+      }
+      
+      state++;
+
+      visitedTwice = visitedTwice || (state == 2 && !IsBigCave);
+
+      int res = 0;
+      foreach(Node n in Nodes)
+      {
+        res += n.Search(visitedTwice);
+      }
+      state--;
+
+      return res;
+    }
+  }
+}
