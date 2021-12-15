92e84ef2eded7afece3d3e1538a4a25481310b09
Ondřej Brožek <obrozek@centrum.cz>
2021-12-15 08:42:37 +0100

    15

---
+++ SolverAOC2021_15/Data.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolverAOC2021_15
+{
+  class Data
+  {
+
+    public List<List<Field>> RiskLevelsMap = new List<List<Field>>();
+    public List<Field> RiskLevelsMapAll = new List<Field>();
+
+    public int Width, Height;
+
+
+    public Data(string input)
+    {
+      using(StringReader sr = new StringReader(input))
+      {
+        string line;
+        while((line = sr.ReadLine()) != null)
+        {
+          RiskLevelsMap.Add(new List<Field>());
+          foreach(char c in line)
+          {
+            int riskLevel = int.Parse(c.ToString());
+            int y = RiskLevelsMap.Count - 1;
+            int x = RiskLevelsMap[y].Count;
+            
+            Field f = new Field(x, y, riskLevel);
+            RiskLevelsMap[y].Add(f);
+            RiskLevelsMapAll.Add(f);
+          }
+        }
+      }
+
+      Height = RiskLevelsMap.Count;
+      Width = RiskLevelsMap[0].Count;
+
+    }
+
+    internal int Solve2()
+    {
+      for(int i = 0; i < Height; i++)
+      {
+        for(int v = 1; v < 5; v++)
+        {
+          for(int j = 0; j < Width; j++)
+          {
+            Field tmp = new Field((Width * v) + j, i, (RiskLevelsMap[i][j].RiskLevel + v - 1) % 9 + 1);
+            RiskLevelsMap[i].Add(tmp);
+            RiskLevelsMapAll.Add(tmp);
+          }
+        }
+      }
+
+      Width = RiskLevelsMap[0].Count;
+      
+      
+      for (int v = 1; v < 5; v++)
+      {
+        for (int i = 0; i < Height; i++)
+        {
+          List<Field> tmpList = new List<Field>();
+          for (int j = 0; j < Width; j++)
+          {
+            Field tmp = new Field(j, i + (v * Height), (RiskLevelsMap[i][j].RiskLevel + v-1) % 9 + 1);
+
+            RiskLevelsMapAll.Add(tmp);
+            tmpList.Add(tmp);
+          }
+          RiskLevelsMap.Add(tmpList);
+        } 
+        
+      }
+
+      Height = RiskLevelsMap.Count;
+
+
+      return Solve1();
+    }
+
+    public int Solve1()
+    {
+      RiskLevelsMap[Height - 1][Width - 1].Init();
+
+
+      List<Field> forSolve = GetFieldsForSolve();
+      while (forSolve.Count > 0)
+      {
+        
+        foreach(Field f in forSolve)
+        {
+          f.Solve(RiskLevelsMap);
+        }
+        
+        forSolve = GetFieldsForSolve();
+      }
+      //PrintRiskLevel();
+      return RiskLevelsMap[0][0].TotalRiskLevel - RiskLevelsMap[0][0].RiskLevel;
+    }
+
+    public void PrintMap()
+    {
+      for (int i = 0; i < Height; i++)
+      {
+        for (int j = 0; j < Width; j++)
+        {
+          Console.Write($"{RiskLevelsMap[i][j].RiskLevel,1}");
+        }
+        Console.WriteLine();
+      }
+      Console.WriteLine();
+    }
+
+    public void PrintRiskLevel()
+    {
+      for (int i = 0; i < Height; i++)
+      {
+        for (int j = 0; j < Width;j++)
+        {
+          Console.Write($"{RiskLevelsMap[i][j].TotalRiskLevel,4}");
+        }
+        Console.WriteLine();
+      }
+      Console.WriteLine();
+    }
+
+
+    private List<Field> GetFieldsForSolve()
+    {
+      List<Field> res = new List<Field>();
+      foreach(Field f in RiskLevelsMapAll.Where(x => x.State == EState.Changed))
+      {
+        f.State = EState.Closed;
+        res.AddRange(f.GetNeighbours(RiskLevelsMap));
+      }
+      return res.Distinct().ToList();
+    }
+       
+
+  }
+}
---
+++ SolverAOC2021_15/Field.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolverAOC2021_15
+{
+  public class Field
+  {
+
+    public int X, Y, RiskLevel;
+
+    public int TotalRiskLevel = int.MaxValue;
+
+    public EState State;
+    public Field(int x, int y, int riskLevel)
+    {
+      X = x;
+      Y = y;
+      RiskLevel = riskLevel;
+      State = EState.Fresh;
+    }
+
+    public void Solve(List<List<Field>> fields)
+    {
+      State = EState.Closed;
+      foreach(Field f in GetNeighbours(fields))
+      {
+        if (f.State == EState.Fresh) continue;
+
+        int tmpRiskLevel = f.TotalRiskLevel + RiskLevel;
+        if (TotalRiskLevel > tmpRiskLevel)
+        {
+          TotalRiskLevel = tmpRiskLevel;
+          State = EState.Changed;
+        }
+      }
+    }
+
+    public List<Field> GetNeighbours(List<List<Field>> fields)
+    {
+      List<Field> res = new List<Field>();
+      for (int i = -1; i <= 1; i ++)
+      {
+        for (int j = -1; j <= 1; j ++)
+        {
+          if (Math.Abs(i) == Math.Abs(j)) continue;
+          int j2 = X + j;
+          int i2 = Y + i;
+          if (i2 < 0 || i2 >= fields.Count) continue;
+          if (j2 < 0 || j2 >= fields[i2].Count) continue;
+          
+          res.Add(fields[i2][j2]);
+        }
+      }
+      return res;
+    }
+
+    public override string ToString()
+    {
+      return $"[{X},{Y}]";
+    }
+
+    internal void Init()
+    {
+      TotalRiskLevel = RiskLevel;
+      State = EState.Changed;
+    }
+  }
+
+  public enum EState
+  {
+    Fresh,
+    Changed,
+    Closed
+  }
+}
